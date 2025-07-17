19def311e1fb0071065aa7c31fadeec95f2a1574
Avish Attri <etherned@gmail.com>
2025-07-18 00:56:28 +0530

    Removed unused useNavigate import to fix Netlify build

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,7 +2,6 @@ import './Home.css'
 import React, { useEffect, useState } from 'react'
 import Card from './Card'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   useEffect(() => {
